9c6a4ea547b37dbb538b538e17f366265eead7a9
Mauricio Reisdoefer <mauricio.reisdoefer2009@gmail.com>
2026-01-28 03:05:21 -0300

    feat: creating renderer

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <SDL3/SDL.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 const int HEIGHT = 500;
 const int WIDTH = 500;
@@ -13,6 +14,18 @@ int main(int argc, char *argv[])
     }
     SDL_Window *window;
     window = SDL_CreateWindow("", WIDTH, HEIGHT, SDL_WINDOW_RESIZABLE);
+
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, NULL);
+
+    // Clears Screen
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    SDL_RenderClear(renderer);
+
+    // Create a White Pixel
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+    SDL_RenderPoint(renderer, WIDTH / 2, HEIGHT / 2);
+
+    SDL_RenderPresent(renderer);
 
     SDL_Delay(5000);
     SDL_Quit();
